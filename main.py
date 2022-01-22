c44887a62a63ae84b6cbce105d33963e0fe405af
Sreehari Pulickamadhom Sreedhar <49713741+orectique@users.noreply.github.com>
2022-01-23 00:19:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -78,69 +78,43 @@ def graph2(num):
 
     return px.bar(out, x = 'Food Type', y = 'Count', title= 'Frequency of Consumption')
 
-
 def graph3():
-    red = []
-    yellow = []
-    green = []
-
-    for i in dietary['Aadhaar'].unique():
-        temp = dietary[dietary['Aadhaar'] == i]
-
-        k = 0
-
-        for ftype in types:
-            if 1 in list(temp[ftype]):
-                k += 1
-            else:
-                k += 0
-
-        if k in range(1, 5):
-            red.append(i)
-        elif k in range(5, 7):
-            yellow.append(i)
-        elif k in range(7, 12):
-            green.append(i)
-
-    fig = go.Figure(data = [go.Bar(
-        x = ['Dietary Diversity : 1-4', 'Dietary Diversity : 5-6', 'Dietary Diversity : 7-11'],
-        y = [len(red), len(yellow), len(green)],
-        marker_color = ['red', 'yellow', 'green']
-    )])
-
-    fig.update_layout(title_text = 'Dietary Diversity')
-
-    return fig, red, yellow, green
-
-def graph5():
     dietary['Sum'] = dietary[['Grains', 'Pulses', 'Other Fruits', 'Leafy Vegetables',
        'Other Vegetables', 'Dairy', 'Meat, Poultry and Fish', 'Vitamin A Rich',
        'Nuts and Seeds', 'Eggs']].sum(axis =1)
-    red = []
+    red1 = []
+    red2 = []
     yellow = []
-    green = []
+    green1 = []
+    green2 = []
 
     for i in dietary['Aadhaar'].unique():
         temp = dietary[dietary['Aadhaar'] == i]
 
         k = ceil(temp['Sum'].mean())
 
-        if k in range(1, 5):
-            red.append(i)
+        if k in range(1, 3):
+            red1.append(i)
+        elif k in range(3, 5):
+            red2.append(i)
         elif k in range(5, 7):
             yellow.append(i)
-        elif k in range(7, 11):
-            green.append(i)
+        elif k in range(7, 10):
+            green1.append(i)
+        elif k in range(10, 11):
+            green2.append(i)
 
     fig = go.Figure(data = [go.Bar(
-        x = ['Dietary Diversity : 1-4', 'Dietary Diversity : 5-6', 'Dietary Diversity : 7-10'],
-        y = [len(red), len(yellow), len(green)],
-        marker_color = ['red', 'yellow', 'green']
+        x = ['Dietary Diversity : 1-2', 'Dietary Diversity : 3-4', 'Dietary Diversity : 5-6', 'Dietary Diversity : 7-9', 'Dietary Diversity : 10-11'],
+        y = [len(red1), len(red2), len(yellow), len(green1), len(green2)],
+        marker_color = ['red', 'red', 'yellow', 'green', 'green']
     )])
 
-    fig.update_layout(title_text = 'Food Groups Consumed, Ceiling of Mean')
+    N = len(dietary['Aadhaar'].unique())
 
-    return fig
+    fig.update_layout(title_text = f'Food Groups Consumed, Ceiling of Mean, N = {N}')
+
+    return fig, red1, red2, yellow, green1, green2
 
 def graph4():
     delta = pd.DataFrame(columns = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
@@ -171,21 +145,20 @@ with head:
         fig4 = graph4()
         st.write(fig4)
 
-    with c5:
-        st.header('Average Number of Food Groups Consumed')
-        fig5 = graph5()
-        st.write(fig5)
-
     with c3:
-        fig3, red, yellow, green = graph3()
+        fig3, red1, red2, yellow, green1, green2 = graph3()
         st.header('Exploration of Dietary Diversity')
-        st.write('In this graphic, a color of red indicates that the diversity of a person\'s diet is below 4 groups. A color of yellow indicates that the diet is restricted to 5 or 6 groups. A color of green indicates a diverse diet that includes more than 6 groups.')
+        st.write('In this graphic, a color of red indicates that the average diversity of a person\'s diet is below 4 groups. A color of yellow indicates that the diet is restricted to 5 or 6 groups. A color of green indicates a diverse diet that includes more than 6 groups.')
         st.write(fig3)
-        with st.expander('People in Red Zone'):
-            st.write(pd.Series(red, index = arange(1, len(red) + 1)), name = 'Aadhaar Numbers')
-        with st.expander('People in Yellow Zone'):
+        with st.expander('People in Red Zone - 1-2'):
+            st.write(pd.Series(red1, index = arange(1, len(red1) + 1)), name = 'Aadhaar Numbers')
+        with st.expander('People in Red Zone - 3-4'):
+            st.write(pd.Series(red2, index = arange(1, len(red2) + 1)), name = 'Aadhaar Numbers')
+        with st.expander('People in Yellow Zone - 5-6'):
             st.write(pd.Series(yellow, index = arange(1, len(yellow) + 1)), name = 'Aadhaar Numbers')
-        with st.expander('People in Green Zone'):
-            st.write(pd.Series(green, index = arange(1, len(green) + 1)), name = 'Aadhaar Numbers')
+        with st.expander('People in Green Zone - 7-9'):
+            st.write(pd.Series(green1, index = arange(1, len(green1) + 1)), name = 'Aadhaar Numbers')
+        with st.expander('People in Green Zone - 10-11'):
+            st.write(pd.Series(green2, index = arange(1, len(green2) + 1)), name = 'Aadhaar Numbers')
         
 
